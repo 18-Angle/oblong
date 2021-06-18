997a815f8fb3fe8e5a57ae20d14636a393ac8b03
Edward Haas <efhiii2000@gmail.com>
2021-06-18 15:06:48 -0700

    clock

---
+++ oblong/assetsList.js
@@ -0,0 +1,39 @@
+const background = new Image();
+background.src = 'assets/background.png';
+
+const board = new Image();
+board.src = 'assets/board.png';
+
+
+const fullscreen = new Image();
+fullscreen.src = 'assets/fullscreen.png';
+const fullscreenb = new Image();
+fullscreenb.src = 'assets/fullscreenb.png';
+
+const exitFullscreen = new Image();
+exitFullscreen.src = 'assets/exitFullscreen.png';
+const exitFullscreenb = new Image();
+exitFullscreenb.src = 'assets/exitFullscreenb.png';
+
+
+const playImg = new Image();
+playImg.src = 'assets/play.png';
+const playImgb = new Image();
+playImgb.src = 'assets/playb.png';
+
+const settingsImg = new Image();
+settingsImg.src = 'assets/settings.png';
+const settingsImgb = new Image();
+settingsImgb.src = 'assets/settingsb.png';
+
+
+const backImg = new Image();
+backImg.src = 'assets/back.png';
+const backImgb = new Image();
+backImgb.src = 'assets/backb.png';
+
+const minus = new Image();
+minus.src = 'assets/minus.png';
+
+const plus = new Image();
+plus.src = 'assets/plus.png';
---
+++ oblong/playNow.js
@@ -0,0 +1,50 @@
+let time;
+let lastTick;
+let clockRunning;
+
+let score = [0, 0];
+
+let quarterText = ["1st", "2nd", "3rd", "4th"];
+
+function startGame(x, y, w, h) {
+  sb = 2;
+  time = 0;
+  lastTick = Date.now();
+  clockRunning = true;
+}
+
+function drawClock(x, y, w, h) {
+  let quarter = time / 300000 >> 0;
+  let clock = time % 300000;
+
+  ctx.textAlign = 'left';
+  ctx.font = `${(w*0.022)>>0}px sans-serif`;
+
+  ctx.fillStyle = '#fff';
+  ctx.fillText(`${clock/60000>>0}:` + `${(clock/1000>>0)%60}`.padStart(2, 0), x + w * 0.7, y + h * 0.1);
+  ctx.fillText(quarterText[quarter], x + w * 0.645, y + h * 0.1);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText(`2nd & 7`, x + w * 0.27, y + h * 0.1);
+
+  ctx.textAlign = 'center'
+  ctx.fillStyle = '#fff';
+  ctx.fillText(score[0], x + w * 0.485, y + h * 0.1);
+  ctx.fillText(score[1], x + w * 0.616, y + h * 0.1);
+}
+
+function drawgame(x, y, w, h) {
+  ctx.drawImage(background, x, y, w, h);
+  ctx.drawImage(board, x, y, w, h);
+  let newTick = Date.now();
+  if(clockRunning) {
+    time += newTick - lastTick;
+  }
+  lastTick = newTick;
+
+  drawClock(x, y, w, h);
+}
+
+function s1() {
+  callWithinAR(0, 0, w, h, 16 / 9, drawgame);
+}
